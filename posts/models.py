363cdb6a7b64af3d1dd3c1c41abba6b7f4ecb815
Juan Marcos Cordobez <juanmarcoscordobez@gmail.com>
2021-06-08 09:23:08 -0300

    requirements.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+"""Posts models."""
 
-# Create your models here.
+# Django
+from django.db import models
+from django.db.models.fields import EmailField
+
+class User(models.Model):
+    """User model. """
+
+    email = models.EmailField()
+    password = models.CharField()
+
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    bio = models.TextField()
+
+    birthdate = models.DateField()
+
+    create = models.DateTimeField()
+    modified = models.DateTimeField()
